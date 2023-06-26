a112ccba54dc546500454ae3c2bc583594d7beee
Muccachino <lucas_staszewski@yahoo.de>
2023-06-26 13:35:16 +0200

    added Homepage completely with JS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,92 @@
 import "./style.scss";
+import logo from "./img/new_cherry_blossom_logo3.jpg";
+import bowl1 from "./img/bowl1.jpg";
+import ramen1 from "./img/ramen1.jpg";
+import sushi3 from "./img/sushi3.jpg";
 
-import { createTags, expandHtml } from "./modules/functions";
+import {
+  createTags,
+  expandHtml,
+  addPicture,
+  createMultipleTags,
+} from "./modules/functions";
 
-const htmlTags = createTags(["div", "h1"]);
-expandHtml(document.body, htmlTags);
+/* Header - Start */
+
+const header = createTags("header", "header");
+expandHtml(document.body.firstElementChild, header);
+
+const headerLogo = addPicture(logo, header);
+
+const headerNavi = createTags("ul", "nav");
+expandHtml(header, headerNavi);
+createMultipleTags("li", 3, headerNavi, [
+  "Home",
+  "Menu",
+  "Gallery",
+  "Contact Us",
+]);
+
+/* Header - End */
+
+/* Mainsection - Start */
+
+const main = createTags("section", null, "main");
+expandHtml(document.body.firstElementChild, main);
+
+const cover = createTags("div", null, "cover");
+expandHtml(main, cover);
+const coverHeadline = createTags("h1", null, null, "Welcome to Sakura");
+expandHtml(cover, coverHeadline);
+
+const foodExamples = createTags("div", null, "food_examples");
+expandHtml(main, foodExamples);
+const foodExamplesHeadline = createTags(
+  "h2",
+  null,
+  null,
+  "Authentic Japanese Dishes"
+);
+expandHtml(foodExamples, foodExamplesHeadline);
+
+const foodPictures = createTags("div", null, "food_pictures");
+expandHtml(foodExamples, foodPictures);
+
+const foodImg1 = addPicture(bowl1, foodPictures);
+const foodImg2 = addPicture(ramen1, foodPictures);
+const foodImg3 = addPicture(sushi3, foodPictures);
+
+/* Mainsection - Ende */
+
+/* Footer - Start */
+
+const footer = createTags("footer", "footer");
+expandHtml(document.body.firstElementChild, footer);
+
+const terms = createTags("div", "terms");
+expandHtml(footer, terms);
+
+const termsLinks = createMultipleTags("a", 1, terms, [
+  "Terms of Use",
+  "Privacy Policy",
+]);
+
+const socialMedia = createTags("div", "socialmedia");
+expandHtml(footer, socialMedia);
+
+const follow = createTags("span", null, null, "Follow us on:");
+expandHtml(socialMedia, follow);
+
+const socialMediaLinks = createMultipleTags("a", 2, socialMedia);
+const socialMediaIcon1 = createTags("i", "fa-brands");
+socialMediaIcon1.classList.add("fa-facebook");
+expandHtml(socialMedia.children[1], socialMediaIcon1);
+
+const socialMediaIcon2 = createTags("i", "fa-brands");
+socialMediaIcon2.classList.add("fa-instagram");
+expandHtml(socialMedia.children[2], socialMediaIcon2);
+
+const socialMediaIcon3 = createTags("i", "fa-brands");
+socialMediaIcon3.classList.add("fa-twitter");
+expandHtml(socialMedia.children[3], socialMediaIcon3);
+/* Footer - End */
--- src/modules/functions.js
+++ src/modules/functions.js
@@ -1,17 +1,42 @@
 "use strict";
 
-function createTags(htmlTags) {
-  let tags = [];
-  for (let item of htmlTags) {
-    tags.push(document.createElement(item));
+import logo from "../img/new_cherry_blossom_logo3.jpg";
+import bowl1 from "../img/bowl1.jpg";
+import ramen1 from "../img/ramen1.jpg";
+import sushi3 from "../img/sushi3.jpg";
+
+function createTags(htmlTags, className, idName, innerHtml) {
+  let element = document.createElement(htmlTags);
+  if (className != null) {
+    element.classList.add(className);
   }
-  return tags;
+  if (idName != null) {
+    element.id = idName;
+  }
+  if (innerHtml != null) {
+    element.innerHTML = innerHtml;
+  }
+  return element;
 }
 
 function expandHtml(expand, tags) {
-  for (let item of tags) {
-    expand.appendChild(item);
+  expand.appendChild(tags);
+}
+
+function addPicture(picImportName, parent) {
+  let myPic = new Image();
+  myPic.src = picImportName;
+  parent.appendChild(myPic);
+}
+
+function createMultipleTags(node, amount, parent, innerHtml) {
+  for (let i = 0; i <= amount; i++) {
+    let tag = document.createElement(node);
+    if (innerHtml != null) {
+      tag.innerHTML = innerHtml[i];
+    }
+    parent.appendChild(tag);
   }
 }
 
-export { createTags, expandHtml };
+export { createTags, expandHtml, addPicture, createMultipleTags };
